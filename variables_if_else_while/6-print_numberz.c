cfcd7ab4d92b0616d833f612d5d8cc167a77753f
Genia888 <khalepo@gmail.com>
2025-02-19 13:59:45 +0000

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+
+	int main(void)
+{
+		char numberz;
+
+		for (numberz = '0'; numberz <= '9'; numberz++)
+		putchar(numberz);
+
+		putchar ('\n');
+
+		return (0);
+
+}
