7d806f99ad145986692c1b5ee24e5bd144424d94
Genia888 <khalepo@gmail.com>
2025-02-21 15:24:38 +0000

    Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_sign - Entry point
+ *@n: number
+ *Return: Always 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+ );
+		return ( );
+	}
+	else if (n < 0)
+	{
+	_putchar (' - ');
+	return (-1);
+	}
+	else
+	{
+	_putchar ('0');
+	return (0);
+	}
+}
