3b845a86104665904c986f346b407871c533e77a
Genia888 <khalepo@gmail.com>
2025-02-25 09:57:13 +0000

    You are so much sunshine in every square inch

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_square - function that prints a square,
+ *followed by a new line
+ *@n: number of times the character
+ *Return: Always 0.
+ */
+
+
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	for (x = 1; x <= n; x++)
+	{
+		for (y = 1; y < x; y++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
