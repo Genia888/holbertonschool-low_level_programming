b9ccd0aab4f74ddc78dd07f471a66a8e80f280ff
Genia888 <khalpo@gmail.com>
2025-02-27 15:51:42 +0000

    A good engineer thinks in reverse and asks himself

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *rev_string - Write a function that reverses a string.
+ *@s: parameter used.
+ *Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int l;
+	int temp;
+	int x;
+	int y;
+
+	l = 0;
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+
+	x = 0;
+	y = l - 1;
+	while (x < y)
+	{
+		temp = s[x];
+		s[x] = s[y];
+		s[y] = temp;
+		x++;
+		y--;
+	}
+}
