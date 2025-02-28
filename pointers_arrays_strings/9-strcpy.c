1506b48ff4886e52a55ebf3c0f5a74d561be34f1
Genia888 <khalepo@gmail.com>
2025-02-28 14:40:56 +0000

    strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcpy - Write a function that copies the string pointed to by src,
+ *including the terminating null byte (\0),
+ *to the buffer pointed to by dest
+ *@src: number
+ *@dest: number
+ *Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int n = 0;
+
+	while (src[n] != '\0')
+	{
+		dest[n] = src[n];
+		n++;
+	}
+	return (dest);
+}
