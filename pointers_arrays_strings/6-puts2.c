3cd11d1961204c71f91da2ad275dcda268cd4bb8
Genia888 <khalepo@gmail.com>
2025-02-28 09:34:02 +0000

    Half the lies they tell about me aren't true

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
  *puts2 - Write a function that prints every other character of a string,
  *starting with the first character, followed by a new line
- *@*str: string
+ *@str:character of string
  *Return: Always 0.
  */
 
