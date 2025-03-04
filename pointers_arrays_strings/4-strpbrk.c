2fdcd0b4d5e575cf395dae781ff370f0ff6d742d
Genia888 <khalepo@gmail.com>
2025-03-04 15:44:30 +0000

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strpbrk - function that searches a string for any of a set of bytes
+ *@s : "hello, world"
+ *@accept : "world"
+ *Return: Return (s + a) or Return (NULL)
+ */
+
+char *_strpbrk(char *s, char *accept)
+
+{
+
+	int a = 0;
+	int b = 0;
+
+	while (s[a] != '\0')
+	{
+		b = 0;
+		while (accept[b] != '\0')
+		{
+			if (s[a] == accept[b])
+				return (s + a);
+			b++;
+		}
+		a++;
+	}
+	return (NULL);
+}
