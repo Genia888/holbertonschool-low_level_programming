d78dd1873e22a28bb6261b94f8b134f8dfd6b0b6
Genia888 <khalepo@gmail.com>
2025-03-14 09:12:20 +0000

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _dog_h_
+#define _dog_h_
+
+/**
+ * struct dog : Define a new type struct dog
+ * @name : dog name
+ * @age : dog age
+ * @owner : dog owner
+ */
+
+typedef struct dog
+
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
