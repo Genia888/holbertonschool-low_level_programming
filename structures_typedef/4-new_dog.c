74dd8deb84afd00a53ddba119cc8f1f1bdcdd94c
Genia888 <khalepo@gmail.com>
2025-03-14 14:34:51 +0000

    A door is what a dog is perpetually on the wrong side of

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,41 +1,52 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
- * new_dog - Creates a new dog.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
+ * new_dog - Crée un nouveau chien.
+ * @name: Nom du chien.
+ * @age: Âge du chien.
+ * @owner: Propriétaire du chien.
  *
- * Return: Pointer to the newly created dog_t structure, or NULL if it fails.
+ * Return: Pointeur vers la nouvelle structure dog_t, ou NULL en cas d'échec.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	char *name_copy, *owner_copy;
+	int name_len = 0, owner_len = 0, i;
+
+	while (name[name_len] != '\0')
+		name_len++;
+
+	while (owner[owner_len] != '\0')
+		owner_len++;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	name_copy = malloc(strlen(name) + 1);
+	name_copy = malloc(name_len + 1);
 	if (name_copy == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(name_copy, name);
 
-
-	owner_copy = malloc(strlen(owner) + 1);
+	owner_copy = malloc(owner_len + 1);
 	if (owner_copy == NULL)
 	{
 		free(name_copy);
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(owner_copy, owner);
+
+
+	for (i = 0; i <= name_len; i++)
+		name_copy[i] = name[i];
+
+
+	for (i = 0; i <= owner_len; i++)
+		owner_copy[i] = owner[i];
 
 
 	new_dog->name = name_copy;
