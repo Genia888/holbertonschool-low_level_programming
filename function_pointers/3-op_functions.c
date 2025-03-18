5fc7004361bde3ac9ef347fa2017798fe106e6e1
Genia888 <khalepo@gmail.com>
2025-03-18 10:39:00 +0000

    save only 3-cal and 3-op-fonction

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <function_pointers.h>
+
+/**
+ * op_add - addition of a and b
+ * @a: number
+ * @b: number
+ * Return: the sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substraction of a and b
+ * @a: number
+ * @b: number
+ * Return: the difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication of a and b
+ * @a: number
+ * @b: number
+ * Return: the product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division of a and b
+ * @a: number
+ * @b: number
+ * Return: the result of the division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of a and b
+ * @a: number
+ * @b: number
+ * Return: the ramainder ot the division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
