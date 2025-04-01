bf72a6f6f514155557640cfa22e0293d2a0860ab
Genia888 <khalepo@gmail.com>
2025-04-01 14:20:08 +0200

    Free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: a pointer 
+ */
+
+void free_list(list_t *head)
+{
+	list_t *clean;
+
+	while (head != NULL)
+	{
+		clean = head->next;
+		free(head->str);
+		free(head);
+		head = clean;
+	}
+}
