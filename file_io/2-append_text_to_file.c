696af81f104db2d4619c9e18d7771a63eec93028
Genia888 <khalepo@gmail.com>
2025-04-04 17:45:49 +0200

    Speak gently, she can hear

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -6,7 +6,7 @@
  * append_text_to_file - function that appends text at the end of a file
  * @filename: name of the file
  * @text_content: text content
- * Return : 1 on success and -1 on failure
+ * Return : 1 on success, -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
