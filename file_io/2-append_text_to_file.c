f545ae40a40634660694de66cb08dae707c54ab0
Genia888 <khalepo@gmail.com>
2025-04-04 17:53:43 +0200

    Speak gently, she can hear

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -5,8 +5,8 @@
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: name of the file to append to
- * @text_content: text to add to the file
- * Return : 1 on success, -1 on failure
+ * @text_content: NULL-terminated string to add to the file
+ * Return: 1 on success, -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
