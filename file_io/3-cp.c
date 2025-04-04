c91ea4ef7eeabd973dbcbfc37ef17082d3740bbd
Genia888 <khalepo@gmail.com>
2025-04-04 22:12:54 +0200

    update main

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #define BUF_SIZE 1024
+
 /**
  * main - Entry point of the program
  * @argc: Number of arguments passed to the program
  * @argv: Array of pointers to the arguments
  *
- * Return: Always 0 on success
+ * Return: 0 on success, or an appropriate exit code on error
  */
 int main(int argc, char *argv[])
 {
-	int fd_from = open(argv[1], O_RDONLY);
-	int fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	int fd_from, fd_to;
 	ssize_t bytes_read, bytes_written;
 	char buf[BUF_SIZE];
 
@@ -19,34 +19,54 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		close(fd_from);
+		exit(98);
 	}
+
 	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buf, bytes_read);
 		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			close(fd_from);
+			close(fd_to);
+			exit(98);
 		}
 	}
+
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
 		exit(98);
 	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
+
+	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", STDERR_FILENO);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
 	return (0);
 }
+
