199b63a8477084081716dbed91fd43c1a0bda6a2
Genia888 <khalepo@gmail.com>
2025-04-04 17:39:17 +0200

    Speak gently, she can hear

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -4,8 +4,8 @@
 
 /**
  * append_text_to_file - function that appends text at the end of a file
- * @filename : name of the file
- * @text_content : text content
+ * @filename: name of the file
+ * @text_content: text content
  * Return : 1 on success and -1 on failure
  */
 
